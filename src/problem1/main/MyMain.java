8abd4dc6dfcd44d0e833d61792fa21a6eb3ad3c2
harsh vardhan <harsh.vardhan_cs18@gla.ac.in>
2020-03-30 22:24:55 +0530

    thank you sir

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -7,5 +7,20 @@
 package problem1.main;
 // executable class
 
+import problem1.mybst.MyBinarySearchTree;
+
 public class MyMain {
+    public static void main(String[] args) {
+        MyBinarySearchTree obj = new MyBinarySearchTree();
+        obj.insert(5);
+        obj.insert(1);
+        obj.insert(10);
+        obj.insert(4);
+        obj.insert(6);
+        System.out.println("ALL LEFT NODES");
+        obj.printLeftNode();
+        System.out.println("Count of Nodes not having left Node");
+        obj.countNotLeft();
+
+    }
 }
--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -5,7 +5,19 @@
  *  Time: 7:13 PM
  */
 package problem3.main;
+
+import problem3.myqueue.MyPriorityQueue;
+
 // executable class
 // use problem5.student.Student class to create object of student
 public class MyMain {
+    public static void main(String[] args) {
+        MyPriorityQueue obj = new MyPriorityQueue();
+        obj.insert("aman", 1);
+        obj.insert("karan", 4);
+        obj.insert("madhav", 3);
+        obj.insert("ishita", 2);
+        obj.insert("prachi", 5);
+        obj.display();
+    }
 }
